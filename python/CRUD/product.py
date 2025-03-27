6bd27413dfa9384f9451e8769fbe55248af410cc
Balintescu <92201004+andreibalintescu123@users.noreply.github.com>
2025-03-27 22:32:35 +0200

    All CRUD operations now have a connection parameter, to establish connections with other data sources and facilitate the reusability of code.

--- python/CRUD/product.py
+++ python/CRUD/product.py
@@ -1,24 +1,22 @@
-from python.database_init import create_connection
+from python.database_init import sqlite_connection
 
 
-def add_product(artisan_id, name, description, price, stock_quantity):
-    conn = create_connection()
-    cursor = conn.cursor()
+def add_product(artisan_id, name, description, price, stock_quantity, connection):
+    cursor = connection.cursor()
     try:
         cursor.execute("INSERT INTO Products (ArtisanID, Name, Description, Price, StockQuantity) VALUES (?, ?, ?, ?, "
                        "?)",
                        (artisan_id, name, description, price, stock_quantity))
-        conn.commit()
+        connection.commit()
     except Exception as e:
         print(f"Error adding product: {e}")
-        conn.rollback()
+        connection.rollback()
     finally:
-        conn.close()
+        connection.close()
 
 
-def get_products():
-    conn = create_connection()
-    cursor = conn.cursor()
+def get_products(connection):
+    cursor = connection.cursor()
     try:
         cursor.execute("SELECT * FROM Products")
         products = cursor.fetchall()
@@ -26,32 +24,30 @@ def get_products():
         print(f"Error getting products: {e}")
         products = []
     finally:
-        conn.close()
+        connection.close()
     return products
 
 
-def update_product(product_id, name, description, price, stock_quantity):
-    conn = create_connection()
-    cursor = conn.cursor()
+def update_product(product_id, name, description, price, stock_quantity, connection):
+    cursor = connection.cursor()
     try:
         cursor.execute("UPDATE Products SET Name=?, Description=?, Price=?, StockQuantity=? WHERE ProductID=?",
                        (name, description, price, stock_quantity, product_id))
-        conn.commit()
+        connection.commit()
     except Exception as e:
         print(f"Error updating product: {e}")
-        conn.rollback()
+        connection.rollback()
     finally:
-        conn.close()
+        connection.close()
 
 
-def delete_product(product_id):
-    conn = create_connection()
-    cursor = conn.cursor()
+def delete_product(product_id, connection):
+    cursor = connection.cursor()
     try:
         cursor.execute("DELETE FROM Products WHERE ProductID=?", (product_id,))
-        conn.commit()
+        connection.commit()
     except Exception as e:
         print(f"Error deleting product: {e}")
-        conn.rollback()
+        connection.rollback()
     finally:
-        conn.close()
+        connection.close()
